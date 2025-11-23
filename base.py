4e03ab1ac4109f308ce914c5803f09ba4b0860b8
some-programmer199 <26jxu@bromsgrove-school.co.uk>
2025-11-23 20:31:29 +0000

    test for MP

--- base.py
+++ base.py
@@ -1,5 +1,14 @@
-import chess
-import torch
-tensor1=torch.rand(3,10)
-tensor2=tensor1[1:]
-print((tensor1.shape,tensor2.shape))
+import lmdb
+import sys
+env=lmdb.open('./lmdb_data',map_size=2**30)
+with env.begin(write=True) as txn:
+    txn.put(b'key', b'value')
+def worker():
+    with env.begin() as txn:
+        value = txn.get(b'key')
+        print(value)
+import multiprocessing
+p = multiprocessing.Process(target=worker)
+p.start()
+p.join()
+    
